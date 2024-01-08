f827775134d7bd9ed8e46c2a9f2baf023aeec122
blue-harry <harryhuang2012@gmail.com>
2024-01-08 20:31:31 +0800

    home route change

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
-import express from "express";
-import { fileURLToPath } from "url";
+import express from "express"
+import { fileURLToPath } from "url"
 
-const app = express();
-app.use("/", express.static(fileURLToPath(new URL("home", import.meta.url))));
-app.use(
-  "/home",
-  express.static(fileURLToPath(new URL("home", import.meta.url)))
-);
+const app = express()
+app.use("/", express.static(fileURLToPath(new URL("home", import.meta.url))))
 
 app.use(
   "/games",
   express.static(fileURLToPath(new URL("games", import.meta.url)))
-);
+)
 app.use(
   "/links",
   express.static(fileURLToPath(new URL("links", import.meta.url)))
-);
+)
 
 //
 app.use(
   "/flappybird",
   express.static(fileURLToPath(new URL("flappybird", import.meta.url)))
-);
+)
 app.use(
   "/pong",
   express.static(fileURLToPath(new URL("pong", import.meta.url)))
-);
+)
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log(`Server is running on port: ${PORT}`));
+const PORT = process.env.PORT || 5000
+app.listen(PORT, console.log(`Server is running on port: ${PORT}`))
